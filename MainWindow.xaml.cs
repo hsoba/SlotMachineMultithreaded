56a7bbe377ad1bd1ac704af6ed8ff145de1b2dfc
H-Soba Vincent <hsobav@gmail.com>
2021-12-13 13:58:49 -0500

    button and text logic, UI updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SlotMachineMultithreaded
 {
@@ -11,6 +15,12 @@ namespace SlotMachineMultithreaded
         BackgroundWorker backgroundWorker2 = new BackgroundWorker();
         BackgroundWorker backgroundWorker3 = new BackgroundWorker();
 
+        SolidColorBrush myGreenBrush = new SolidColorBrush(Color.FromRgb(208, 255, 214));
+        SolidColorBrush myRedBrush = new SolidColorBrush(Color.FromRgb(211, 63, 73));
+        SolidColorBrush myBlackBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
+        int winCounter = 1;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -29,19 +39,20 @@ namespace SlotMachineMultithreaded
             backgroundWorker1.WorkerReportsProgress = true;
             backgroundWorker2.WorkerReportsProgress = true;
             backgroundWorker3.WorkerReportsProgress = true;
+
         }
 
         private void BgWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            this.lblSlot_1.Content = e.ProgressPercentage;
+            lblSlot_1.Content = e.ProgressPercentage;
         }
         private void BgWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            this.lblSlot_2.Content = e.ProgressPercentage;
+            lblSlot_2.Content = e.ProgressPercentage;
         }
         private void BgWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            this.lblSlot_3.Content = e.ProgressPercentage;
+            lblSlot_3.Content = e.ProgressPercentage;
         }
 
         private void BgWorker_Work1(object sender, DoWorkEventArgs e)
@@ -86,7 +97,7 @@ namespace SlotMachineMultithreaded
             }
         }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
             if (backgroundWorker1.IsBusy || backgroundWorker2.IsBusy || backgroundWorker3.IsBusy)
             {
@@ -97,6 +108,9 @@ namespace SlotMachineMultithreaded
                 if (!backgroundWorker1.IsBusy)
                 {
                     backgroundWorker1.RunWorkerAsync();
+                    btn1.Content = "Stop";
+                    btn1.Foreground = myRedBrush;
+                    btn1.FontWeight = FontWeights.Heavy;
                 }
                 else
                 {
@@ -106,6 +120,9 @@ namespace SlotMachineMultithreaded
                 if (!backgroundWorker2.IsBusy)
                 {
                     backgroundWorker2.RunWorkerAsync();
+                    btn2.Content = "Stop";
+                    btn2.Foreground = myRedBrush;
+                    btn2.FontWeight = FontWeights.Heavy;
                 }
                 else
                 {
@@ -115,22 +132,42 @@ namespace SlotMachineMultithreaded
                 if (!backgroundWorker3.IsBusy)
                 {
                     backgroundWorker3.RunWorkerAsync();
+                    btn3.Content = "Stop";
+                    btn3.Foreground = myRedBrush;
+                    btn3.FontWeight = FontWeights.Heavy;
                 }
                 else
                 {
                     CancelAllWorkers();
                 }
-                btnStart.Content = "Stop";
+                btnPlay.Content = "Stop";
+                btnPlay.Foreground = myRedBrush;
+                btnPlay.FontWeight = FontWeights.Heavy;
+                //btnPlay.Background = myRedBrush;
             }
         }
 
         private void CancelAllWorkers()
         {
-            backgroundWorker1.CancelAsync();
-            backgroundWorker2.CancelAsync();
-            backgroundWorker3.CancelAsync();
+            //btnPlay.Background = btn1.Background = btn2.Background = btn3.Background = myGreenBrush;
+            btnPlay.Content = "Play";
+            btnPlay.Foreground = myBlackBrush;
+            btnPlay.FontWeight = FontWeights.Medium;
 
-            btnStart.Content = "Start";
+            backgroundWorker1.CancelAsync();
+            btn1.Content = "Hit";
+            btn1.Foreground = myBlackBrush;
+            btn1.FontWeight = FontWeights.Medium;
+
+            backgroundWorker2.CancelAsync();
+            btn2.Content = "Hit";
+            btn2.Foreground = myBlackBrush;
+            btn2.FontWeight = FontWeights.Medium;
+
+            backgroundWorker3.CancelAsync();
+            btn3.Content = "Hit";
+            btn3.Foreground = myBlackBrush;
+            btn3.FontWeight = FontWeights.Medium;
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
@@ -139,17 +176,23 @@ namespace SlotMachineMultithreaded
             {
                 backgroundWorker1.RunWorkerAsync();
 
-                btnStart.Content = "Stop";
+                btnPlay.Content = "Stop";
                 btn1.Content = "Stop";
+                btnPlay.Foreground = btn1.Foreground = myRedBrush;
+                btnPlay.FontWeight = btn1.FontWeight = FontWeights.Heavy;
             }
             else
             {
                 backgroundWorker1.CancelAsync();
                 if (!backgroundWorker2.IsBusy && !backgroundWorker3.IsBusy)
                 {
-                    btnStart.Content = "Start";
+                    btnPlay.Content = "Play"; 
+                    btnPlay.Foreground = myBlackBrush;
+                    btnPlay.FontWeight = FontWeights.Medium;
                 }
                 btn1.Content = "Hit";
+                btn1.Foreground = myBlackBrush;
+                btn1.FontWeight = FontWeights.Medium;
             }
         }
 
@@ -159,17 +202,23 @@ namespace SlotMachineMultithreaded
             {
                 backgroundWorker2.RunWorkerAsync();
 
-                btnStart.Content = "Stop";
+                btnPlay.Content = "Stop";
                 btn2.Content = "Stop";
+                btnPlay.Foreground = btn2.Foreground = myRedBrush;
+                btnPlay.FontWeight = btn2.FontWeight = FontWeights.Heavy;
             }
             else
             {
                 backgroundWorker2.CancelAsync();
                 if (!backgroundWorker1.IsBusy && !backgroundWorker3.IsBusy)
                 {
-                    btnStart.Content = "Start";
+                    btnPlay.Content = "Play";
+                    btnPlay.Foreground = myBlackBrush;
+                    btnPlay.FontWeight = FontWeights.Medium;
                 }
                 btn2.Content = "Hit";
+                btn2.Foreground = myBlackBrush;
+                btn2.FontWeight = FontWeights.Medium;
             }
         }
 
@@ -179,23 +228,42 @@ namespace SlotMachineMultithreaded
             {
                 backgroundWorker3.RunWorkerAsync();
 
-                btnStart.Content = "Stop";
+                btnPlay.Content = "Stop";
                 btn3.Content = "Stop";
+                btnPlay.Foreground = btn3.Foreground = myRedBrush;
+                btnPlay.FontWeight = btn3.FontWeight = FontWeights.Heavy;
             }
             else
             {
                 backgroundWorker3.CancelAsync();
                 if (!backgroundWorker1.IsBusy && !backgroundWorker2.IsBusy)
                 {
-                    btnStart.Content = "Start";
+                    btnPlay.Content = "Play";
+                    btnPlay.Foreground = myBlackBrush;
+                    btnPlay.FontWeight = FontWeights.Medium;
                 }
                 btn3.Content = "Hit";
+                btn3.Foreground = myBlackBrush;
+                btn3.FontWeight = FontWeights.Medium;
             }
         }
 
         private int Random_Num_Generate()
         {
             return new Random().Next(0, 9);
+        }
+
+        private void btnCashIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblSlot_1.Content.ToString() == lblSlot_2.Content.ToString() && lblSlot_2.Content.ToString() == lblSlot_3.Content.ToString())
+            {
+                lblWins.Content = "Wins: " + winCounter++.ToString();
+                MessageBox.Show("Winner...");
+            }
+            else
+            {
+                MessageBox.Show("Try again");
+            }
         }
     }
 }
